4b1d5dca36ba6a0bcf29ab8c33829090d4934446
BayuAnggoroSakti <bayu.anggoro.s@mail.ugm.ac.id>
2022-08-08 08:26:32 +0700

    Update recaptcha.php
    ganti key captcha

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -3,8 +3,8 @@
 // To use reCAPTCHA, you need to sign up for an API key pair for your site.
 // link: http://www.google.com/recaptcha/admin
 // key captcha puragroup
-$config['recaptcha_site_key'] = '6Lfq5q4ZAAAAAIuyoGT7PBiAW7Q8y0R95MKsH0sa';
-$config['recaptcha_secret_key'] = '6Lfq5q4ZAAAAAFV8hivBffHjjQStHjcfR1xWhFSr';
+$config['recaptcha_site_key'] = '6LfFYlchAAAAAN8kACOf8fcW-J4fV7vG0wCh2lwN';
+$config['recaptcha_secret_key'] = '6LfFYlchAAAAABJpC24-4_x-waEzJJ5h6PnHFkDS';
 
 
 // reCAPTCHA supported 40+ languages listed here:
