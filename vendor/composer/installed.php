8246f8d8194720d60eff0db1dcc225f6878841a1
BayuAnggoroSakti <bayu.anggoro.s@mail.ugm.ac.id>
2022-08-08 09:37:08 +0700

    dsfdsf

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '58c911eb431c8070f5f02f936bb7644dd8954a7b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '58c911eb431c8070f5f02f936bb7644dd8954a7b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
